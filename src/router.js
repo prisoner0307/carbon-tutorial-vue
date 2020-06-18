ba6577958527beee0d5a3ce9b034945043457716
Xi Yuan <yuanxiy@cm.ibm.com>
2020-06-18 14:49:44 +0800

    feat(tutorial): complete step 1

--- src/router.js
+++ src/router.js
@@ -12,11 +12,8 @@ export default new Router({
     {
       path: "/repos",
       name: "repo-page",
-      // route level code-splitting
-      // this generates a separate chunk (repo-page.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "repo-page" */ "./views/RepoPage"),
+        import("./views/RepoPage"),
     },
   ],
 });
